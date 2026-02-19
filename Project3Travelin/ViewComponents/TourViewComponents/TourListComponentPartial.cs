6e27628d1b92e334e5adf1ec7046bf45422c2c79
MuratYucedag <yucedagmurat23@gmail.com>
2026-02-19 14:15:40 +0300

    ...

---
+++ Project3Travelin/ViewComponents/TourViewComponents/TourListComponentPartial.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project3Travelin.Services.TourServices;
+
+namespace Project3Travelin.ViewComponents.TourViewComponents
+{
+    public class TourListComponentPartial : ViewComponent
+    {
+        private readonly ITourService _tourService;
+        public TourListComponentPartial(ITourService tourService)
+        {
+            _tourService = tourService;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var values = await _tourService.GetAllTourAsync();
+            return View(values);
+        }
+    }
+}
